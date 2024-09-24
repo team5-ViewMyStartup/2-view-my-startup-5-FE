ee334a8efaf5a73296dad427b25c7dd56e49fd30
hyobiin9 <moone1234@kakao.com>
2024-09-24 23:56:14 +0900

    feat: Landing
    - Landing ui 개발
    - Pages 요소 추가(LandingMain 불러오는 js)

---
+++ src/component/LandingMain.jsx
@@ -0,0 +1,21 @@
+// TODO: 추후에 Link 사용하여 login 페이지 이동 기능 추가해야함
+
+import React from "react";
+import Styles from "./LandingMain.module.css";
+import mainImg from "../assets/logo1.svg";
+import { Link } from "react-router-dom";
+
+const LandingMain = () => {
+  return (
+    <div className={Styles.landing_main_container}>
+      <img className={Styles.landing_main_img} src={mainImg} alt="main-image" />
+      <div className={Styles.landing_main_text}>
+        <p className={Styles.landing_main_already}>이미 계정이 있다면?</p>
+        <p className={Styles.landing_main_login}>로그인하러 가기</p>
+      </div>
+      <button className={Styles.landing_main_around_button}>둘러보기</button>
+    </div>
+  );
+};
+
+export default LandingMain;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingMain from "../component/LandingMain";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <LandingMain />
+    </div>
+  );
+}
+
+export default HomePage;
