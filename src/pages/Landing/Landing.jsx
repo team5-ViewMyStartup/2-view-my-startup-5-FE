d60c56a929b87ab809b8d81d8e3bad2e2dc5759c
hyobiin9 <moone1234@kakao.com>
2024-09-25 16:57:29 +0900

    fix: 헤더랑 랜딩 합쳤더니 나타나는 오류 수정 / feat: 회원가입 페이지 추가

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,19 +1,25 @@
-// TODO: 추후에 Link 사용하여 login 페이지 이동 기능 추가해야함
+/**
+ * TODO
+ * 1. 추후에 Link 사용하여 둘러보기 버튼 눌렀을 시 전체기업페이지로 이동기능 추가
+ * 2. 로그인하러 가기 글자 눌렀을 시 path login으로 바꾸기(회원가입 페이지 정상작동하는지 확인차 바꿔둠)
+ */
 
 import React from "react";
-import Styles from "./Landing.module.css";
-import mainImg from "../assets/logo1.svg";
+import styles from "./Landing.module.css";
+import mainImg from "../../assets/logo1.svg";
 import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div className={Styles.landing_main_container}>
-      <img className={Styles.landing_main_img} src={mainImg} alt="main-image" />
-      <div className={Styles.landing_main_text}>
-        <p className={Styles.landing_main_already}>이미 계정이 있다면?</p>
-        <p className={Styles.landing_main_login}>로그인하러 가기</p>
+    <div className={styles.landing_main_container}>
+      <img className={styles.landing_main_img} src={mainImg} alt="main-image" />
+      <div className={styles.landing_main_text}>
+        <p className={styles.landing_main_already}>이미 계정이 있다면?</p>
+        <Link to="/signup">
+          <p className={styles.landing_main_login}>로그인하러 가기</p>
+        </Link>
       </div>
-      <button className={Styles.landing_main_around_button}>둘러보기</button>
+      <button className={styles.landing_main_around_button}>둘러보기</button>
     </div>
   );
 }
