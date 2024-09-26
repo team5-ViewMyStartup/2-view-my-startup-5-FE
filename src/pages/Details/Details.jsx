e2c98bf1e35466939e2260cf1aa13f8ef606f06a
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-09-26 10:12:47 +0900

    fix: api part

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./Details.module.css";
 
 function Details() {
-  const [startup, setStartup] = useState();
+  const [company, setCompany] = useState();
   // TODO: fetch 작업
   // useEffect(() => {
   //   fetch("http://localhost:5000/api/startup/미정")
@@ -19,7 +19,8 @@ function Details() {
       totalInvestment: 140,
       revenue: 44.3,
       employees: 95,
-      description: "코드잇은 '온라인 코딩 교육 서비스'를 운영하는 EdTech 스타트업입니다.",
+      description:
+        "코드잇은 '온라인 코딩 교육 서비스'를 운영하는 EdTech 스타트업입니다. 코딩 교육과 데이터 사이언스 교육에 대한 수요는 급격히 늘어나고 있지만, 아직까지 좋은 교육 서비스를 찾기란 쉽지 않습니다. 이를 해결하고자 코드잇은 모든 강의를 자체 제작하여 퀄리티 높은 콘텐츠를 제공하고, 동시에 코딩 교육에 최적화된 플랫폼을 개발하고 있습니다. 모든 강의를 마음껏 들을 수 있는 코드잇 무제한 멤버십을 제공하고 있으며, 지난 5년 동안 21만 명의 수강생과 평균 만족도 4.9점이라는 국내 교육 업계에서 보기 드문 성과를 달성하였습니다. 또한 콘텐츠와 기술력을 인정받아 2021년 10월 Series B 투자를 받아 누적 140억 원의 투자를 받았고, 현재 40여 명의 팀원이 같은 목표를 향해 나아가고 있습니다. “배움의 기쁨을 세상 모두에게.” 이것이 코드잇의 비전입니다. 현재는 최고의 코딩 교육 서비스를 국내에서 제공하고 있지만, 이보다 더 큰 그림을 그리고 있습니다. 2021년 상반기부터 영어권 시장 진출을 시작했고, 코딩과 인접한 분야부터 스펙트럼을 넓혀 나갈 계획입니다.",
       investments: [
         {
           investorName: "김연우",
@@ -54,11 +55,11 @@ function Details() {
       ],
     };
     setTimeout(() => {
-      setStartup(mockData);
+      setCompany(mockData);
     }, 1000);
   }, []);
 
-  if (!startup) {
+  if (!company) {
     return <div>데이터 불러오지 못했습니다</div>;
   }
 
@@ -67,22 +68,22 @@ function Details() {
       {/* TODO: 1. api를 받아와야함 2. 페이지네이션 */}
       <div className={styles.corporate_information}>
         <div className={styles.corporate_name}>
-          <h3>{startup.name}</h3>
-          <h4 className={styles.cor_type}>{startup.category}</h4> <hr />
+          <h3>{company.name}</h3>
+          <h4 className={styles.cor_type}>{company.category}</h4> <hr />
         </div>
         <div className={styles.corporate_status}>
           <div className={styles.overview_wrapper}>
             <div className={styles.overview}>
               <h5>누적 투자 금액</h5>
-              <p>{startup.totalInvestment}억 원</p>
+              <p>{company.totalInvestment}억 원</p>
             </div>
             <div className={styles.overview}>
               <h5>매출액</h5>
-              <p>{startup.revenue} 억 원</p>
+              <p>{company.revenue} 억 원</p>
             </div>
             <div className={styles.overview}>
               <h5>고용 인원</h5>
-              <p>{startup.employees} 명</p>
+              <p>{company.employees} 명</p>
             </div>
           </div>
           <div className={styles.introduction}>
@@ -110,7 +111,7 @@ function Details() {
             </tr>
           </thead>
           <tbody>
-            {startup.investments.map((investment, index) => (
+            {company.investments.map((investment, index) => (
               <tr key={index}>
                 <td>{investment.investorName}</td>
                 <td>{investment.rank} 위</td>
