d6b400c723fec5cec10d35c4a5e560b17b6a435b
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-09-26 09:02:32 +0900

    feat: temporary data update

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,28 +1,88 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Details.module.css";
 
 function Details() {
+  const [startup, setStartup] = useState();
+  // TODO: fetch 작업
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/api/startup/미정")
+  //     .then((response) => response.json())
+  //     .then((data) => setStartup(data))
+  //     .catch((error) => console.error("Error fetching data:", error));
+  // }, []);
+
+  // FIXME: 임시 데이터
+  useEffect(() => {
+    const mockData = {
+      name: "코드잇",
+      category: "에듀테크",
+      totalInvestment: 140,
+      revenue: 44.3,
+      employees: 95,
+      description: "코드잇은 '온라인 코딩 교육 서비스'를 운영하는 EdTech 스타트업입니다.",
+      investments: [
+        {
+          investorName: "김연우",
+          rank: 1,
+          amount: 10,
+          comment: "코드잇은 정말 훌륭한 기업입니다!",
+        },
+        {
+          investorName: "이유지",
+          rank: 2,
+          amount: 9,
+          comment: "코드잇은 정말 훌륭한 기업입니다!",
+        },
+        {
+          investorName: "안다해",
+          rank: 3,
+          amount: 8,
+          comment: "코드잇은 정말 훌륭한 기업입니다!",
+        },
+        {
+          investorName: "이유지",
+          rank: 4,
+          amount: 7,
+          comment: "코드잇은 정말 훌륭한 기업입니다!",
+        },
+        {
+          investorName: "이유지",
+          rank: 5,
+          amount: 6,
+          comment: "코드잇은 정말 훌륭한 기업입니다!",
+        },
+      ],
+    };
+    setTimeout(() => {
+      setStartup(mockData);
+    }, 1000);
+  }, []);
+
+  if (!startup) {
+    return <div>데이터 불러오지 못했습니다</div>;
+  }
+
   return (
     <div className={styles.corporate}>
       {/* TODO: 1. api를 받아와야함 2. 페이지네이션 */}
       <div className={styles.corporate_information}>
         <div className={styles.corporate_name}>
-          <h3>코드잇</h3>
-          <h4 className={styles.cor_type}>에듀테크</h4> <hr />
+          <h3>{startup.name}</h3>
+          <h4 className={styles.cor_type}>{startup.category}</h4> <hr />
         </div>
         <div className={styles.corporate_status}>
           <div className={styles.overview_wrapper}>
             <div className={styles.overview}>
               <h5>누적 투자 금액</h5>
-              <p>억 원</p>
+              <p>{startup.totalInvestment}억 원</p>
             </div>
             <div className={styles.overview}>
               <h5>매출액</h5>
-              <p>억 원</p>
+              <p>{startup.revenue} 억 원</p>
             </div>
             <div className={styles.overview}>
               <h5>고용 인원</h5>
-              <p> 명</p>
+              <p>{startup.employees} 명</p>
             </div>
           </div>
           <div className={styles.introduction}>
@@ -50,18 +110,14 @@ function Details() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>dafs</td>
-              <td>dfaf</td>
-              <td>dafs</td>
-              <td>dfaf</td>
-            </tr>
-            <tr>
-              <td>dafs</td>
-              <td>dfaf</td>
-              <td>dafs</td>
-              <td>dfaf</td>
-            </tr>
+            {startup.investments.map((investment, index) => (
+              <tr key={index}>
+                <td>{investment.investorName}</td>
+                <td>{investment.rank} 위</td>
+                <td>{investment.amount} 억 원</td>
+                <td>{investment.comment}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
