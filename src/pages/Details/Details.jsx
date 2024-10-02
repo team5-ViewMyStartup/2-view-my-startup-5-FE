ea4d5998b1515db6119c104bd345bf70a3c9075a
unknown <didrkgus0426@gmail.com>
2024-10-02 22:16:06 +0900

    fix: list header per type width size complete

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -66,192 +66,194 @@ function Details() {
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
-  };
 
-  const handlePrevious = () => {
-    if (currentPage > 1) setCurrentPage(currentPage - 1);
-  };
+    const handlePrevious = () => {
+      if (currentPage > 1) setCurrentPage(currentPage - 1);
+    };
 
-  const handleNext = () => {
-    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
-  };
+    const handleNext = () => {
+      if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    };
 
-  const toggleDropdown = (index) => {
-    setActiveDropdown(activeDropdown === index ? null : index);
-  };
+    const toggleDropdown = (index) => {
+      setActiveDropdown(activeDropdown === index ? null : index);
+    };
 
-  const handleImgClick = (index) => {
-    toggleDropdown(index);
-  };
+    const handleImgClick = (index) => {
+      toggleDropdown(index);
+    };
 
-  const corporateField = [
-    {
-      title: "누적 투자 금액",
-      value: `${totalInvestmentAmount} 억 원`,
-      className: "",
-    },
-    {
-      title: "매출액",
-      value: `${company.revenue} 억 원`,
-      className: `${styles.account}`,
-    },
-    {
-      title: "고용 인원",
-      value: `${company.employees} 명`,
-      className: "",
-    },
-  ];
+    const corporateField = [
+      {
+        title: "누적 투자 금액",
+        value: `${totalInvestmentAmount} 억 원`,
+        className: "",
+      },
+      {
+        title: "매출액",
+        value: `${company.revenue} 억 원`,
+        className: `${styles.account}`,
+      },
+      {
+        title: "고용 인원",
+        value: `${company.employees} 명`,
+        className: "",
+      },
+    ];
 
-  /** TODO
-   * 1. map 형식으로 바꾸기
-   * 2. pagination
-   * 3. api 연결
-   * 4. 수정/삭제 모달 확인
-   * 5. 연결 안될 시 화면 (완료)
-   * 6. 삭제 실패 팝업...
-   */
+    /** TODO
+     * 1. map 형식으로 바꾸기
+     * 2. pagination
+     * 3. api 연결
+     * 4. 수정/삭제 모달 확인
+     * 5. 연결 안될 시 화면 (완료)
+     * 6. 삭제 실패 팝업...
+     */
 
-  return (
-    <div className={styles.corporate}>
-      <div className={styles.corporate_information}>
-        <div className={styles.corporate_name}>
-          <h3>{company.name}</h3>
-          <h4 className={styles.cor_type}>{company.category}</h4> <hr />
-        </div>
-        <div className={styles.corporate_status}>
-          <div className={styles.overview_wrapper}>
-            {corporateField.map((field, index) => (
-              <div key={index} className={`${styles.overview} ${field.className}`}>
-                <h5>{field.title}</h5>
-                <p>{field.value}</p>
-              </div>
-            ))}
+    return (
+      <div className={styles.corporate}>
+        <div className={styles.corporate_information}>
+          <div className={styles.corporate_name}>
+            <h3>{company.name}</h3>
+            <h4 className={styles.cor_type}>{company.category}</h4> <hr />
           </div>
-          <div className={styles.introduction}>
-            <h5>기업 소개</h5>
-            <p>{company.description}</p>
-          </div>
-        </div>
-      </div>
-      <div className={styles.investment_received}>
-        <div className={styles.invest_wrapper}>
-          <h3>View My Startup에서 받은 투자</h3>
-          <button className={styles.invest_button}>기업투자하기</button>
-        </div>
-        <hr />
-        {company.investments.length === 0 ? (
-          <div className={styles.no_investment}>
-            <p>아직 투자한 기업이 없어요.</p>
-            <p>버튼을 눌러 기업에 투자해보세요!</p>
-          </div>
-        ) : (
-          <>
-            <div>
-              <h3>총 {totalInvestmentAmount} 억원</h3>
-            </div>
-            <div className={styles.investment_container}>
-              <ul className={styles.investment_list}>
-                <li className={styles.investment_header}>
-                  <span className={styles.invest_inform}>투자자 이름</span>
-                  <span className={styles.invest_inform}>순위</span>
-                  <span className={styles.invest_inform}>투자 금액</span>
-                  <span className={styles.investment_comment}>투자 코멘트</span>
-                </li>
-                {currentInvestments.map((investment, index) => (
-                  <li key={index + indexOfFirstItem} className={styles.investment_item}>
-                    <span className={styles.invest_inform}>{investment.investorName}</span>
-                    <span className={styles.invest_inform}>{index + indexOfFirstItem + 1} 위</span>
-                    <span className={styles.invest_inform}>{investment.amount} 억 원</span>
-                    <span className={styles.comment_content}>{investment.comment}</span>
-                    <span className={styles.select_box}>
-                      <div onClick={() => handleImgClick(index + indexOfFirstItem)}>
-                        <img src={select_icon} alt="select icon" className={styles.select_img} />
-                      </div>
-                      {activeDropdown === index + indexOfFirstItem && (
-                        <div className={styles.dropdown_select}>
-                          <div className={styles.dropbox_item}>
-                            <ul>
-                              <li
-                                className={styles.dropbox_item}
-                                onClick={() => openEditModal(investment)}
-                              >
-                                수정하기
-                              </li>
-                              <div className={styles.line}></div>
-                              <li
-                                className={`${styles.dropbox_item} ${styles.dropbox_item_last}`}
-                                onClick={() => openDeleteModal(investment)}
-                              >
-                                삭제하기
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      )}
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className={styles.pagination}>
-              <button
-                className={styles.page_button}
-                onClick={handlePrevious}
-                disabled={currentPage === 1}
-              >
-                &lt;
-              </button>
-              {Array.from({ length: totalPages }, (_, index) => (
-                <button
-                  key={index + 1}
-                  className={`${styles.page_button} ${
-                    currentPage === index + 1 ? styles.active : ""
-                  }`}
-                  onClick={() => handlePageChange(index + 1)}
-                >
-                  {index + 1}
-                </button>
+          <div className={styles.corporate_status}>
+            <div className={styles.overview_wrapper}>
+              {corporateField.map((field, index) => (
+                <div key={index} className={`${styles.overview} ${field.className}`}>
+                  <h5>{field.title}</h5>
+                  <p>{field.value}</p>
+                </div>
               ))}
-              <button
-                className={styles.page_button}
-                onClick={handleNext}
-                disabled={currentPage === totalPages}
-              >
-                &gt;
-              </button>
             </div>
-          </>
+            <div className={styles.introduction}>
+              <h5>기업 소개</h5>
+              <p>{company.description}</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.investment_received}>
+          <div className={styles.invest_wrapper}>
+            <h3>View My Startup에서 받은 투자</h3>
+            <button className={styles.invest_button}>기업투자하기</button>
+          </div>
+          <hr />
+          {company.investments.length === 0 ? (
+            <div className={styles.no_investment}>
+              <p>아직 투자한 기업이 없어요.</p>
+              <p>버튼을 눌러 기업에 투자해보세요!</p>
+            </div>
+          ) : (
+            <>
+              <div>
+                <h3>총 {totalInvestmentAmount} 억원</h3>
+              </div>
+              <div className={styles.investment_container}>
+                <ul className={styles.investment_list}>
+                  <li className={styles.investment_header}>
+                    <span className={styles.invest_inform}>투자자 이름</span>
+                    <span className={styles.invest_inform}>순위</span>
+                    <span className={styles.invest_inform}>투자 금액</span>
+                    <span className={styles.investment_comment}>투자 코멘트</span>
+                  </li>
+                  {currentInvestments.map((investment, index) => (
+                    <li key={index + indexOfFirstItem} className={styles.investment_item}>
+                      <span className={styles.invest_inform}>{investment.investorName}</span>
+                      <span className={styles.invest_inform}>
+                        {index + indexOfFirstItem + 1} 위
+                      </span>
+                      <span className={styles.invest_inform}>{investment.amount} 억 원</span>
+                      <span className={styles.comment_content}>{investment.comment}</span>
+                      <span className={styles.select_box}>
+                        <div onClick={() => handleImgClick(index + indexOfFirstItem)}>
+                          <img src={select_icon} alt="select icon" className={styles.select_img} />
+                        </div>
+                        {activeDropdown === index + indexOfFirstItem && (
+                          <div className={styles.dropdown_select}>
+                            <div className={styles.dropbox_item}>
+                              <ul>
+                                <li
+                                  className={styles.dropbox_item}
+                                  onClick={() => openEditModal(investment)}
+                                >
+                                  수정하기
+                                </li>
+                                <div className={styles.line}></div>
+                                <li
+                                  className={`${styles.dropbox_item} ${styles.dropbox_item_last}`}
+                                  onClick={() => openDeleteModal(investment)}
+                                >
+                                  삭제하기
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        )}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className={styles.pagination}>
+                <button
+                  className={styles.page_button}
+                  onClick={handlePrevious}
+                  disabled={currentPage === 1}
+                >
+                  &lt;
+                </button>
+                {Array.from({ length: totalPages }, (_, index) => (
+                  <button
+                    key={index + 1}
+                    className={`${styles.page_button} ${
+                      currentPage === index + 1 ? styles.active : ""
+                    }`}
+                    onClick={() => handlePageChange(index + 1)}
+                  >
+                    {index + 1}
+                  </button>
+                ))}
+                <button
+                  className={styles.page_button}
+                  onClick={handleNext}
+                  disabled={currentPage === totalPages}
+                >
+                  &gt;
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+        {deleteModalOpen && (
+          <DeleteModal
+            isOpen={deleteModalOpen}
+            isClose={closeDeleteModal}
+            investment={selectedInvestment}
+          />
+        )}
+        {editModalOpen && (
+          <EditModal
+            isOpen={editModalOpen}
+            isClose={closeEditModal}
+            investment={selectedInvestment}
+            onSave={(updatedInvestment) => {
+              const updatedInvestments = company.investments.map((invest) =>
+                invest.id === updatedInvestment.id
+                  ? { ...invest, comment: updatedInvestment.comment }
+                  : invest,
+              );
+              setCompany((prevCompany) => ({
+                ...prevCompany,
+                investments: updatedInvestments,
+              }));
+              closeEditModal();
+            }}
+          />
         )}
       </div>
-      {deleteModalOpen && (
-        <DeleteModal
-          isOpen={deleteModalOpen}
-          isClose={closeDeleteModal}
-          investment={selectedInvestment}
-        />
-      )}
-      {editModalOpen && (
-        <EditModal
-          isOpen={editModalOpen}
-          isClose={closeEditModal}
-          investment={selectedInvestment}
-          onSave={(updatedInvestment) => {
-            const updatedInvestments = company.investments.map((invest) =>
-              invest.id === updatedInvestment.id
-                ? { ...invest, comment: updatedInvestment.comment }
-                : invest,
-            );
-            setCompany((prevCompany) => ({
-              ...prevCompany,
-              investments: updatedInvestments,
-            }));
-            closeEditModal();
-          }}
-        />
-      )}
-    </div>
-  );
+    );
+  };
 }
 
 export default Details;
