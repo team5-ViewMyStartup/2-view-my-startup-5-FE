d2456ec54453e9e83a19af26d668f4ab7b18668c
junbaehyun <reasonmylife@naver.com>
2024-10-02 09:19:22 +0900

    feat:toggling completed

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,51 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Signup.module.css";
 import logoImg from "../../assets/logo1.svg";
-import toggle from "../../assets/btn_visibility_off_24px.svg";
+import toggleOff from "../../assets/btn_visibility_off_24px.svg";
+import toggleOn from "../../assets/btn_visibility_on_24px.png";
 import { useState } from "react";
 
-/**
- *TODO
- * 유효성 검사를 위한 사용자 입력 데이터 상태 관리
- * error 입력 검증 결과 상태 관리
- * 사용자 입력 즉시 반영하여 유저 경험 개선
- */
 function Signup() {
+  /**
+   *TODO
+   * 유효성 검사를 위한 사용자 입력 데이터 상태 관리/ error 입력 검증 결과 상태 관리
+   * 토글 on, off 상태관리
+   */
   const [formData, setFormData] = useState({
     email: "",
     nickname: "",
     password: "",
     confirmPassword: "",
   });
-
   const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
 
   /**
    *TODO
-   * 입력 필드 변화 처리 함수 정의
-   * 사용자 입력 즉시 반영하여 유저 경험 개선
+   * 입력 필드 변화 처리 함수 정의 /사용자 입력 즉시 반영하여 유저 경험 개선
+   * 폼 제출 처리 함수 정의/ 폼 제출 후 새로 고침 방지
+   * 유효성 검사 함수 정의/  토글 on, off 함수 정의
+   * 유효성 검사 통과 시 회원가입 처리 로직 구현 / 회원가입 성공 시 화면 이동
    */
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData({ ...formData, [id]: value });
   };
-
-  /**
-   *TODO
-   * 폼 제출 처리 함수 정의
-   * 폼 제출 후 새로 고침 방지
-   */
-
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validateForm()) {
       console.log("고생하셨어요. 회원가입에 성공하셨습니다!");
     }
   };
-  /**
-   *TODO
-   * 유효성 검사 함수 정의
-   */
+
   const validateForm = () => {
     const newErrors = {};
 
@@ -71,6 +63,10 @@ function Signup() {
 
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -113,12 +109,17 @@ function Signup() {
             className={styles.pw_input}
             id="password"
             placeholder="비밀번호를 입력해주세요"
-            type="password"
+            type={showPassword ? "text" : "password"}
             value={formData.password}
             onChange={handleChange}
           />
           {errors.password && <p className={styles.error_message}>{errors.password}</p>}
-          <img className={styles.toggle_img} src={toggle} alt="eye Image" type="password" />
+          <img
+            className={styles.toggle_img}
+            src={showPassword ? toggleOff : toggleOn}
+            alt="eye Image"
+            onClick={togglePasswordVisibility}
+          />
         </div>
         <div className={styles.pw_check}>
           <label className={styles.pw_check_label} htmlFor="confirmPassword">
@@ -128,13 +129,19 @@ function Signup() {
             className={styles.pw_check_input}
             id="confirmPassword"
             placeholder="비밀번호를 다시 한 번 입력해주세요"
+            type={showPassword ? "text" : "password"}
             value={formData.confirmPassword}
             onChange={handleChange}
           />
           {errors.confirmPassword && (
             <p className={styles.error_message}>{errors.confirmPassword}</p>
           )}
-          <img className={styles.toggle_img} src={toggle} alt="eye Image" type="password" />
+          <img
+            className={styles.toggle_img}
+            src={showPassword ? toggleOff : toggleOn}
+            alt="eye Image"
+            onClick={togglePasswordVisibility}
+          />
         </div>
         <button className={styles.signup_button} type="submit">
           회원가입
