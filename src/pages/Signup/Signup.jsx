1fad47cf5e1207c67e24e7b84fb9eb75ced25bee
junbaehyun <reasonmylife@naver.com>
2024-10-02 18:22:25 +0900

    feat: dynamic validation

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,17 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Signup.module.css";
 import logoImg from "../../assets/logo1.svg";
 import toggleOff from "../../assets/btn_visibility_off_24px.svg";
 import toggleOn from "../../assets/btn_visibility_on_24px.png";
-import { useState } from "react";
 
 function Signup() {
-  /**
-   *TODO
-   * 유효성 검사를 위한 사용자 입력 데이터 상태 관리/ error 입력 검증 결과 상태 관리
-   * 토글 on, off 상태관리
-   */
   const [formData, setFormData] = useState({
     email: "",
     nickname: "",
@@ -20,47 +14,69 @@ function Signup() {
   });
   const [errors, setErrors] = useState({});
   const [showPassword, setShowPassword] = useState(false);
+  const [passwordStrength, setPasswordStrength] = useState("");
 
-  /**
-   *TODO
-   * 입력 필드 변화 처리 함수 정의 /사용자 입력 즉시 반영하여 유저 경험 개선
-   * 폼 제출 처리 함수 정의/ 폼 제출 후 새로 고침 방지
-   * 유효성 검사 함수 정의/  토글 on, off 함수 정의
-   * 유효성 검사 통과 시 회원가입 처리 로직 구현 / 회원가입 성공 시 화면 이동
-   */
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData({ ...formData, [id]: value });
+
+    const newErrors = { ...errors };
+    switch (id) {
+      case "email":
+        if (!value) {
+          newErrors.email = "이메일을 입력해주세요 :)";
+        } else if (!/\S+@\S+\.\S+/.test(value)) {
+          newErrors.email = "이메일 형식을 확인해주세요 :)";
+        } else {
+          delete newErrors.email;
+        }
+        break;
+
+      case "nickname":
+        if (!value) {
+          newErrors.nickname = "닉네임을 입력해주세요 :)";
+        } else {
+          delete newErrors.nickname;
+        }
+        break;
+
+      case "password":
+        if (!value) {
+          newErrors.password = "비밀번호를 입력해주세요 :)";
+        } else if (
+          !/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/.test(value)
+        ) {
+          newErrors.password =
+            "비밀번호는 최소 8자 이상이며, 대문자, 소문자, 숫자 및 특수문자를 포함해야 해요!";
+        } else {
+          delete newErrors.password;
+        }
+        break;
+
+      case "confirmPassword":
+        if (value !== formData.password) {
+          newErrors.confirmPassword = "앗! 비밀번호가 일치하지 않아요 :)";
+        } else {
+          delete newErrors.confirmPassword;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    setErrors(newErrors);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validateForm()) {
-      console.log("고생하셨어요. 회원가입에 성공하셨습니다!");
+      console.log("회원가입에 성공하셨습니다!");
     }
   };
 
   const validateForm = () => {
     const newErrors = {};
-
-    if (!formData.email) {
-      newErrors.email = `이메일을 입력해주세요 :)`;
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      newErrors.email = "이메일 형식을 확인해주세요!";
-    }
-    if (!formData.nickname) {
-      newErrors.nickname = "닉네임을 입력해주세요";
-    }
-
-    if (!formData.password) {
-      newErrors.password = "비밀번호를 입력해주세요";
-    } else if (formData.password.length < 6) {
-      newErrors.password = "비밀번호는 최소 6자 이상이어야 해요!";
-    }
-
-    if (formData.password !== formData.confirmPassword) {
-      newErrors.confirmPassword = `앗! 비밀번호가 일치하지 않아요:)`;
-    }
-
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
@@ -99,7 +115,6 @@ function Signup() {
             value={formData.nickname}
             onChange={handleChange}
           />
-          {errors.nickname && <p className={styles.error_message}>{errors.nickname}</p>}
         </div>
         <div className={styles.pw}>
           <label className={styles.pw_label} htmlFor="password">
