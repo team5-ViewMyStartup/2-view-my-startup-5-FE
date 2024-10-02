1c2136fa0291e0989eaa48768adcccfe519168a4
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-10-02 09:05:41 +0900

    feat: api udate

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,14 @@
+const BASE_URL = "http://localhost:3000";
+
 export const fetchCompanyData = async (companyId) => {
-  const response = await fetch(`/companies/${companyId}`);
+  const response = await fetch(`${BASE_URL}//companies/${companyId}`);
   if (!response.ok) throw new Error("기업 데이터를 불러오지 못했습니다");
   return await response.json();
 };
 
 export const fetchInvestmentsData = async (companyId) => {
-  const response = await fetch(`/investments/${companyId}`);
+  //const base_url = local:3000
+  const response = await fetch(`${BASE_URL}/investments/${companyId}`);
   if (!response.ok) throw new Error("투자 데이터를 불러오지 못했습니다");
   return await response.json();
 };
