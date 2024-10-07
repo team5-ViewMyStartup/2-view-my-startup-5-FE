241889f025f61b637c1610a4aaf997a080ee03fd
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-10-07 11:06:10 +0900

    feat: sorted to filter data update

--- src/pages/StartupList/StartupList.jsx
+++ src/pages/StartupList/StartupList.jsx
@@ -35,20 +35,42 @@ function StartupList() {
   };
 
   useEffect(() => {
-    const sorted = [...company].sort((a, b) => b.totalInvestment - a.totalInvestment);
-    setSortedData(sorted);
-
-    const filtered = sorted.filter((info) => {
-      const lowerCaseQuery = searchQuery.toLowerCase();
+    const lowerCaseQuery = searchQuery.toLowerCase();
+    const filtered = company.filter((info) => {
       return (
         info.name.toLowerCase().includes(lowerCaseQuery) ||
         info.category.toLowerCase().includes(lowerCaseQuery)
       );
     });
 
-    setFilteredData(filtered);
-    setCurrentPage(1);
-  }, [company, searchQuery]);
+    let sorted = filtered;
+    switch (orderBy) {
+      case "investment-high":
+        sorted = filtered.sort((a, b) => b.totalInvestment - a.totalInvestment);
+        break;
+      case "investment-low":
+        sorted = filtered.sort((a, b) => a.totalInvestment - b.totalInvestment);
+        break;
+      case "sales-high":
+        sorted = filtered.sort((a, b) => b.revenue - a.revenue);
+        break;
+      case "sales-low":
+        sorted = filtered.sort((a, b) => a.revenue - b.revenue);
+        break;
+      case "employeeNum-high":
+        sorted = filtered.sort((a, b) => b.employees - a.employees);
+        break;
+      case "employeeNum-low":
+        sorted = filtered.sort((a, b) => a.employees - b.employees);
+        break;
+      default:
+        sorted = filtered;
+        break;
+    }
+
+    setFilteredData(sorted);
+    setCurrentPage(1); // 검색 시 첫 페이지로 리셋
+  }, [company, searchQuery, orderBy]);
 
   const totalPages = Math.ceil(filteredData.length / viewCompanyInfoNum);
   const indexOfLastItem = currentPage * viewCompanyInfoNum;
