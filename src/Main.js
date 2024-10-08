a5a070027979ab4df5d658f6f79e979d715d55db
junbaehyun <reasonmylife@naver.com>
2024-10-08 10:13:22 +0900

    fix: MockData without preselecting

--- src/Main.js
+++ src/Main.js
@@ -5,10 +5,9 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import StartupList from "./pages/StartupList/StartupList";
 import Details from "./pages/Details/Details";
-import CompareCompany from "./pages/CompareCompany/CompareCompany";
-import Investment from "./pages/InvestmentStatus/Investment";
 import CompareStatus from "./pages/CompareStatus/CompareStatus";
-import CompareResult from "./pages/CompareResult/CompareResult";
+import Investment from "./pages/InvestmentStatus/Investment";
+import CompareCompany from "./pages/CompareCompany/CompareCompany";
 
 function Main() {
   return (
@@ -20,11 +19,10 @@ function Main() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/compare" element={<CompareCompany />} />
           <Route path="/all-company" element={<StartupList />} />
-          <Route path="details" element={<Details />} />
-          {/* <Route path="/compare-status" element={<CompareStatus />} /> */}
+          <Route path="/details/:companyId" element={<Details />} />
           <Route path="/invest-status" element={<Investment />} />
           <Route path="/compare-status" element={<CompareStatus />} />
-          <Route path="/compare-result" element={<CompareResult />} />
+
           {/* <Route> 여기에 페이지들을 추가</Route> */}
         </Route>
       </Routes>
