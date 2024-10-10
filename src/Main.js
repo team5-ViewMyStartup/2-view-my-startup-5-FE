6b447d8e70e44c2dd8bf3afd1299b2dbfd03beaf
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-10-10 15:36:42 +0900

    feat: 404 error page update

--- src/Main.js
+++ src/Main.js
@@ -9,6 +9,7 @@ import CompareStatus from "./pages/CompareStatus/CompareStatus";
 import Investment from "./pages/InvestmentStatus/Investment";
 import Compare from "./pages/CompareStatus/CompareStatus";
 import CompareResult from "./pages/CompareResult/CompareResult";
+import NotFoundPage from "./components/NotFoundPage";
 
 function Main() {
   return (
@@ -24,6 +25,7 @@ function Main() {
           <Route path="/invest-status" element={<Investment />} />
           <Route path="/compare-status" element={<CompareStatus />} />
           <Route path="/compare-result" element={<CompareResult />} />
+          <Route path="*" element={<NotFoundPage />} />
           {/* <Route> 여기에 페이지들을 추가</Route> */}
         </Route>
       </Routes>
